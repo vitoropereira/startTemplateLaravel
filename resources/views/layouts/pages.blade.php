b2b8963106fa683e6ea54f691547ec3cab139e24
Vitor <vop1234@hotmail.com>
2022-02-21 00:13:26 -0300

    Added button component

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -146,10 +146,11 @@
                   class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                   Login
               </a>
-              <a href="{{ route('register') }}" 
-              class="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700">
+              
+              <x-butons.primary href="{{ route('register') }}" >
+
                   Cadastre-se
-              </a>
+            </x-butons.primary>
           </div>
       </div>
 
